2378ac94d56ca0444a0e25a7580094cd70383d84
Ben Granas <benjamingranas@gmail.com>
2016-07-20 12:18:14 -0700

    option migration

--- db/migrate/20160628175600_create_competitor_price_options.rb
+++ db/migrate/20160628175600_create_competitor_price_options.rb
@@ -2,7 +2,8 @@ class CreateCompetitorPriceOptions < ActiveRecord::Migration
   def up
     create_table :competitor_price_options do |t|
     	t.references :competitor_price
-    	t.string :name
+    	t.string :header
+    	t.string :title
     	t.string :price
       t.timestamps null: false
     end
